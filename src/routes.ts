dd62e44cd1c5b2c347fb3eb4c332e163f79e3bb6
Evandro Jun Ishikawa <evandroishikawa@usp.br>
2020-06-04 02:50:26 -0300

    Refactor to use controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,17 @@
 import express from 'express';
 
+import ItemsController from './controllers/items_controller';
+import PointsController from './controllers/points_controller';
+
 const routes = express.Router();
+
+const itemsController = new ItemsController();
+const pointsController = new PointsController();
+
+routes.get('/items', itemsController.index);
+
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+routes.post('/points', pointsController.create);
 
 export default routes;
