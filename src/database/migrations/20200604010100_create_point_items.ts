2815e28c8651c0b533861334e3bf0193006a7f0c
Evandro Jun Ishikawa <evandroishikawa@usp.br>
2020-06-04 01:12:43 -0300

    create base project structure

---
+++ src/database/migrations/20200604010100_create_point_items.ts
@@ -0,0 +1,16 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable('point_items', table => {
+    table.increments('id').primary();
+
+    table.string('point_id').notNullable().references('id').inTable('points');
+    table.string('item_id').notNullable().references('id').inTable('items');
+  });
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTable('point_items');
+}
